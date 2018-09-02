2b8a7deb2f9e570f2ff8c2877c876188e182becb
kguddeta <kguddeta@yahoo.com>
2018-09-02 05:07:50 -0500

    Arquillian JUnit4 Test

---
+++ src/test/com/kguddeta/bookstore/repository/BookRepositoryTest.java
@@ -0,0 +1,65 @@
+package com.kguddeta.bookstore.repository;
+
+import com.kguddeta.bookstore.model.Book;
+import com.kguddeta.bookstore.model.Language;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import javax.inject.Inject;
+import java.util.Date;
+
+import static org.junit.Assert.*;
+
+@RunWith(Arquillian.class)
+public class BookRepositoryTest {
+    @Inject
+    private BookRepository bookRepository;
+
+    @Test
+    public void create() {
+
+        //Test counting books
+        assertEquals(Long.valueOf(0),bookRepository.countAll());
+        assertEquals(0,bookRepository.findAll().size());
+        Book book = new Book("isbn", "a title", 12F, 123, Language.ENGLISH, new Date(), "http://image_url", "description");
+        book = bookRepository.create(book);
+        Long bookId = book.getId();
+
+        //Check created book
+        assertNotNull(bookId);
+
+        //Find created book
+        Book bookFound = bookRepository.find(bookId);
+
+        //check the found book
+        assertEquals("a title",bookFound.getTitle());
+
+        //Test counting books
+        assertEquals(Long.valueOf(1),bookRepository.countAll());
+        assertEquals(1,bookRepository.findAll().size());
+
+        //Delete the book
+        bookRepository.delete(bookId);
+
+        //Test counting books
+        assertEquals(Long.valueOf(0),bookRepository.countAll());
+        assertEquals(0,bookRepository.findAll().size());
+
+    }
+
+    @Deployment
+    public static JavaArchive createDeployment() {
+        return ShrinkWrap.create(JavaArchive.class)
+                .addClass(BookRepository.class)
+                .addClass(Book.class)
+                .addClass(Language.class)
+                .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml")
+                .addAsManifestResource("META-INF/persistence.xml", "persistence.xml");
+    }
+
+}
