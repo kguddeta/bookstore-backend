70d14af2491e11c53275970cc80bb3d0a40e6e6e
kguddeta <kguddeta@yahoo.com>
2018-09-02 03:50:15 -0500

    Transaction Annotation added

--- src/main/java/com/kguddeta/bookstore/repository/BookRepository.java
+++ src/main/java/com/kguddeta/bookstore/repository/BookRepository.java
@@ -5,18 +5,28 @@ import com.kguddeta.bookstore.model.Book;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
+import javax.transaction.Transactional;
 import java.util.List;
 
+import static javax.transaction.Transactional.TxType.REQUIRED;
+import static javax.transaction.Transactional.TxType.SUPPORTS;
+
+@Transactional(SUPPORTS)
 public class BookRepository {
     @PersistenceContext(unitName = "bookStorePU")
     private EntityManager em;
+
     public Book find(Long id){
         return em.find(Book.class,id);
     }
+
+    @Transactional(REQUIRED)
     public Book create(Book book){
         em.persist(book);
         return book;
     }
+    
+    @Transactional(REQUIRED)
     public  void  delete(Long id){
         em.remove(em.getReference(Book.class,id));
     }
